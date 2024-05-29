bc7da73b217919ce55501c1e38c4ddfc5fded75c
Lucas <Lucas.Sampaio@jala.university>
2024-05-29 15:07:55 -0300

    Incluindo classe conta com Getters e Setters

---
+++ ContaBanco/src/conta.java
@@ -0,0 +1,36 @@
+public class conta {
+    private int agencia;
+    private int conta;
+    private String nomeCliente;
+    private double saldo;
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public int getAgencia() {
+        return agencia;
+    }
+    public int getConta() {
+        return conta;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void depositar(double valor){
+        this.saldo =+ valor;
+    }
+
+
+
+}
