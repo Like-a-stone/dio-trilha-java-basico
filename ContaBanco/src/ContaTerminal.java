6089825dfc7b4de2bd1159038a56a1663d71da76
Lucas <Lucas.Sampaio@jala.university>
2024-05-29 14:32:26 -0300

    Processo de criação de conta.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int agencia = scanner.nextInt();
+        System.out.println("Por favor, digite o número da Conta !");
+        int conta = scanner.nextInt();
+        System.out.println("Por favor, digite seu nome !");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite seu saldo !");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo "+saldo+" já está disponível para saque");
+
+        scanner.close();
     }
 }
